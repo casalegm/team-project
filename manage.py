269b7ff466cdb175808b2179c6a5e14ec4bf785f
casalegm <casalegm@gmail.com>
2017-12-04 22:20:17 -0500

    added database information

--- manage.py
+++ manage.py
@@ -1,7 +1,25 @@
 from flask_script import Manager
-from project import app, db
+from project import app, db, Network, Show
 
 manager = Manager(app)
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    CW = Network(name='The CW', description='CW Television Network')
+    ABC = Network(name='ABC', description='American Broadcasting Company Television Network')
+    NBC = Network(name='NBC', description='The National Broadcasting Company')
+    Greys = Show(title='Greys Anatomy', year=2005, description='The medical drama series focuses on a group of doctors at a hospital in Seattle, including several who began their careers at the facility as interns.', network=ABC)
+    Supernatural = Show(title='Supernatural', year=2005, description='This haunting series follows the thrilling yet terrifying journeys of Sam and Dean Winchester, two brothers who face an increasingly sinister landscape as they hunt monsters.', network=CW)
+    Friends = Show(title='Friends', year=1994, description='Three young men and three young women - of the BFF kind - live in the same apartment complex and face life and love in New York. ', network=NBC)
+    db.session.add(CW)
+    db.session.add(ABC)
+    db.session.add(NBC)
+    db.session.add(Greys)
+    db.session.add(Supernatural)
+    db.session.add(Friends)
+    db.session.commit()
 
 
 if __name__ == "__main__":
--- project.py
+++ project.py
@@ -10,6 +10,22 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 db = SQLAlchemy(app)
 
+class Network(db.Model):
+    __tablename__ = 'networks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    shows = db.relationship('Show', backref='network')
+
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    network_id = db.Column(db.Integer, db.ForeignKey('networks.id'))
+
 
 @app.route('/')
 def index():
@@ -21,14 +37,16 @@ def members_page():
     return render_template('members.html')
 
 
-@app.route('/tvshows')
+@app.route('/shows')
 def shows_page():
-    return render_template('shows.html')
+    shows = Show.query.all()
+    return render_template('tvshow-all.html', shows=shows)
 
 
 @app.route('/networks')
 def networks_page():
-    return render_template('networks.html')
+    networks = Network.query.all()
+    return render_template('network-all.html', networks=networks)
 
 
 if __name__ == '__main__':
